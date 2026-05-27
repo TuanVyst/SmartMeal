50b81fbd9762878e45fafb6867fee266a1686ccc
Phạm Gia Huy <chickenhuy2005@gmail.com>
2026-05-27 22:29:56 +0700

    Feat: Account relative entities

---
+++ BusinessObject/Entities/Subscription.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Entities
+{
+    [Table("Subscription")]
+    public class Subscription
+    {
+        [Key]
+        public int Sub_id { get; set; }
+
+        [ForeignKey("Account")]
+        public int Account_id { get; set; }
+
+        [ForeignKey("Plan")]
+        public int Plan_id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        /// <summary>
+        /// active | expired | cancelled
+        /// </summary>
+        [Required]
+        [MaxLength(20)]
+        public string Status { get; set; }
+
+        [MaxLength(255)]
+        public string PaymentRef { get; set; }
+
+        // Navigation properties
+        public virtual Account Account { get; set; }
+        public virtual Plan Plan { get; set; }
+    }
+}
---
+++ BusinessObject/Entities/UserInformation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Entities
+{
+    [Table("UserInformation")]
+    public class UserInformation
+    {
+        [Key]
+        public int User_id { get; set; }
+
+        [ForeignKey("Account")]
+        public int Account_id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [MaxLength(255)]
+        public string Address { get; set; }
+
+        // Navigation properties
+        public virtual Account Account { get; set; }
+    }
+}
