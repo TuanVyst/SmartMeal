fe05f2f7f89628e9622b31a17067babe2de6be26
Phạm Gia Huy <chickenhuy2005@gmail.com>
2026-05-27 22:39:17 +0700

    Feat: add entity relative to community and basic entity for recipe

---
+++ BusinessObject/Entities/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Entities
+{
+    [Table("Comment")]
+    public class Comment
+    {
+        [Key]
+        public int Comment_id { get; set; }
+
+        [ForeignKey("Post")]
+        public int Post_id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        /// <summary>Null if top-level comment, set if reply</summary>
+        public int? Reply_id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsEdited { get; set; } = false;
+
+        // Navigation properties
+        public virtual Post Post { get; set; }
+
+        [ForeignKey("Reply_id")]
+        public virtual Comment ParentComment { get; set; }
+        public virtual ICollection<Comment> Replies { get; set; }
+        public virtual ICollection<Report> Reports { get; set; }
+    }
+}
---
+++ BusinessObject/Entities/Report.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Entities
+{
+    [Table("Report")]
+    public class Report
+    {
+        [Key]
+        public int Report_id { get; set; }
+
+        [ForeignKey("Comment")]
+        public int Comment_id { get; set; }
+
+        [ForeignKey("Post")]
+        public int Post_id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        // Navigation properties
+        public virtual Comment Comment { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
