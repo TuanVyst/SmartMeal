1ddd1b4bdc959bbbdd52340f45eb89cfa270ce21
Phạm Gia Huy <chickenhuy2005@gmail.com>
2026-05-28 23:08:40 +0700

    Feat: Ingredient Repo

---
+++ Repository/Interfaces/IIngredientTagRepo.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IIngredientTagRepo
+    {
+            Task<List<IngredientTag>> GetAllIngredientTags();
+            Task<IngredientTag?> GetIngredientTagById(Guid id);
+            Task<IngredientTag> CreateIngredientTag(IngredientTag ingredientTag);
+            Task<IngredientTag> UpdateIngredientTag(IngredientTag ingredientTag);
+            Task<IngredientTag> SoftDeleteIngredientTag(Guid id);
+    }
+}
